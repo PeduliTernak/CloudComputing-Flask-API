cdcbca31fbbca2f1f73eb1e9c74b7a6f33189900
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-12-06 17:42:59 +0700

    implements prediction

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+from predict import predict_with_model
+
 app = Flask(__name__)
 CORS(app)
 
 
 def perform_prediction(image_bytes):
-  return 'not implemented'
+  return predict_with_model(image_bytes).tolist()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -22,6 +24,7 @@ def index():
       result = perform_prediction(image_bytes)
       return jsonify({'status': True, 'prediction': result})
     except Exception as e:
+      print(e)
       return jsonify({'status': False, 'error': str(e)})
 
   return jsonify({'status': True, 'message': 'OK'})
---
+++ helpers.py
@@ -0,0 +1,48 @@
+import io
+from PIL import Image as pil_image
+
+
+def image_load_img_manual(
+  image_bytes,
+  target_size=None,
+  interpolation="nearest",
+):
+  """Load image from bytes.
+
+  Args:
+    image_bytes: Bytes of image.
+    target_size: Target size.
+
+  Source:
+    tf.keras.utils.load_img
+    https://github.com/keras-team/keras/blob/v2.14.0/keras/utils/image_utils.py
+
+  """
+  img = pil_image.open(io.BytesIO(image_bytes))
+
+  try:
+    pil_image_resampling = pil_image.Resampling
+  except AttributeError:
+    pil_image_resampling = pil_image
+
+  if pil_image_resampling is not None:
+    _PIL_INTERPOLATION_METHODS = {
+        "nearest": pil_image_resampling.NEAREST,
+        "bilinear": pil_image_resampling.BILINEAR,
+        "bicubic": pil_image_resampling.BICUBIC,
+        "hamming": pil_image_resampling.HAMMING,
+        "box": pil_image_resampling.BOX,
+        "lanczos": pil_image_resampling.LANCZOS,
+    }
+
+  if target_size is not None:
+    width_height_tuple = (target_size[1], target_size[0])
+    if img.size != width_height_tuple:
+
+      resample = _PIL_INTERPOLATION_METHODS[interpolation]
+
+      # `if keep_aspect_ratio:` deleted
+      # assuming `keep_aspect_ratio` is False
+      img = img.resize(width_height_tuple, resample)
+
+  return img
